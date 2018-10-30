24ec706c7e8cada764f9a75a967fcd4224e328ca
Dillon Buchanan <thedillonb@gmail.com>
2018-10-30 11:25:41 -0400

    Missed a default export

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
 declare class Tooltip {
-	constructor(content?: string | Element, options?: any);
+  constructor(content?: string | Element, options?: any);
 
-	content(content: string | Element): Tooltip;
-	attach(element: Element): Tooltip;
-	detach(): Tooltip;
-	show(): Tooltip;
-	hide(): Tooltip;
+  content(content: string | Element): Tooltip;
+  attach(element: Element): Tooltip;
+  detach(): Tooltip;
+  show(): Tooltip;
+  hide(): Tooltip;
 }
+
+export default Tooltip;
