3970958a4cad13e5efb8ab57fa41254ca2eb39c1
Chris Main <cmain@128technology.com>
2019-04-11 12:51:12 -0400

    Fix Typings

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,25 @@
-declare class Tooltip {
-  element: Element;
-  hidden: boolean;
-  constructor(content?: string | Element, options?: any);
+declare module '@128technology/darsain-tooltip' {
+  declare class Tooltip {
+    element: Element;
+    hidden: boolean;
+    options: any;
+    classes: any;
+    constructor(content?: string | Element, options?: any);
 
-  content(content: string | Element): Tooltip;
-  attach(element: Element): Tooltip;
-  detach(): Tooltip;
-  show(): Tooltip;
-  hide(): Tooltip;
+    content(content: string | Element): Tooltip;
+    attach(element: Element): Tooltip;
+    detach(): Tooltip;
+    show(): Tooltip;
+    hide(): Tooltip;
+    type(name: string): Tooltip;
+    effect(name: string): Tooltip;
+    changeClassType(propName: string, newClass: string): Tooltip;
+    updateSize(): Tooltip;
+    place(place: string): Tooltip;
+    position(x: number | Element, y: number): Tooltip;
+    toggle(): Tooltip;
+    destroy(): void;
+  }
+
+  export = Tooltip;
 }
-
-export default Tooltip;
